ffd9cdba981339660fa1f0861ba9532eaf870d53
Johann Folta <jfolta@kyruus.com>
2021-02-18 13:21:07 -0500

    handling errors

--- app.js
+++ app.js
@@ -5,16 +5,28 @@ const url =
   "http://api.weatherstack.com/current?access_key=79e93368ece76c99d0d3b786b317e05a&query=Boston&units=f";
 
 request({ url: url, json: true }, (error, response) => {
-  console.log(
-    `It is currently ${response.body.current.temperature} F. It feels like ${response.body.current.feelslike} F. `
-  );
+  if (error) {
+    console.log("Unable to connect to weather service!");
+  } else if (response.body.error) {
+    console.log("Unable to find location");
+  } else {
+    console.log(
+      `It is currently ${response.body.current.temperature} F. It feels like ${response.body.current.feelslike} F. `
+    );
+  }
 });
 
-// Geocoding
+// Geocoding (Address -> Lat/Long)
 const geocodeUrl =
   "https://api.mapbox.com/geocoding/v5/mapbox.places/boston.json?access_token=pk.eyJ1IjoiY29tcGxleGFwaWNlbnN1c2FuZG1hcCIsImEiOiJjanh6ZnBlYWEwMmptM2RvYW02ZTIwODk0In0.m4zyrwu_-34qVZNFVbKtCQ&limit=1";
 request({ url: geocodeUrl, json: true }, (error, response) => {
-  const latitude = response.body.features[0].center[1];
-  const longitude = response.body.features[0].center[0];
-  console.log(latitude, longitude);
+  if (error) {
+    console.log("Unable to connect to geocode location!");
+  } else if (response.body.features.length === 0) {
+    console.log("Unable to find location. Try another search.");
+  } else {
+    const latitude = response.body.features[0].center[1];
+    const longitude = response.body.features[0].center[0];
+    console.log(latitude, longitude);
+  }
 });
